03e201c998470adae4a9c5a5c94d832db7ba6d1c
JanKulesza <151505076+JanKulesza@users.noreply.github.com>
2023-11-24 10:36:41 +0100

    Update loginValidation.php

--- Assignment-2/loginValidation.php
+++ Assignment-2/loginValidation.php
@@ -1,23 +1,11 @@
 <?php
-require_once('db_config.php');
-
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Sprawdź, czy istnieją dane w formularzu
-    if (isset($_POST["login"]) && isset($_POST["password"])) {
-        // Przygotuj zapytanie SQL
-        $sql = "SELECT * FROM users WHERE username = '{$_POST["login"]}' AND password = '{$_POST["password"]}'";
-        $result = $conn->query($sql);
-
-        // Sprawdź, czy wynik zapytania zawiera dokładnie jeden rekord (użytkownik)
-        if ($result->num_rows == 1) {
-            echo "<p>Poprawne logowanie!</p>";
-        } else {
-            echo "<p>Błędny login lub hasło. Spróbuj ponownie.</p>";
-        }
-    } else {
-        echo "<p>Nieprawidłowe dane wejściowe.</p>";
+    $login = "admin";
+    $haslo = "test";
+    if($login == $_POST["login"] and $haslo == $_POST["haslo"]){
+        echo "Pomyślne logowanie";
+    }else{
+        echo "Niepoprawny login lub hasło";
     }
 }
-
-$conn->close();
 ?>
